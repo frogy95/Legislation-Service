851658770bc42c300277ebdd1c8aa132d35ea10c
frogy95 <frogy95@ubcare.co.kr>
2024-12-13 14:46:38 +0900

    리펙토링(w/copilot)

--- rssmaker/DbHandler.py
+++ rssmaker/DbHandler.py
@@ -20,43 +20,52 @@ class DbHandler:
     def insert(self, article):
         # 데이터베이스에 기사(article)를 삽입하는 메서드
         cur = self.conn.cursor()
-        sql = """Insert Into rss(title ,link, description, 
-        item_title, item_link, item_description, item_author, item_category, item_pubDate, item_guid)
-        values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"""
-        ret = cur.execute(sql, (article.title, article.link, article.description,
-                                article.item_title, article.item_link, article.item_description,
-                                article.item_author, article.item_category, article.item_pubDate, article.item_guid))
+        existing_article = cur.execute("SELECT * FROM rss WHERE item_guid = ?", (article.item_guid,)).fetchone()
+        if existing_article:
+            sql = """UPDATE rss SET
+            title = ?,
+            link = ?,
+            description = ?,
+            item_title = ?,
+            item_link = ?,
+            item_description = ?,
+            item_author = ?,
+            item_category = ?,
+            item_pubDate = ?
+            WHERE item_guid = ?"""
+        else:
+            sql = """INSERT INTO rss(title, link, description, 
+            item_title, item_link, item_description, item_author, item_category, item_pubDate, item_guid)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"""
+
+        cur.execute(sql, (article.title, article.link, article.description,
+                          article.item_title, article.item_link, article.item_description,
+                          article.item_author, article.item_category, article.item_pubDate, article.item_guid))
+        
         self.conn.commit()
-        print(ret)
 
     def get_max_id(self, title):
         # 주어진 제목(title)에 해당하는 가장 큰 item_guid 값을 가져오는 메서드
         cur = self.conn.cursor()
-        cur.execute("select item_guid from rss where title = '{0}' order by id desc limit 1;".format(title))
-        rows = cur.fetchall()
-        if len(rows) > 0:
-            max_id = int(rows[0]['item_guid'])
-        else:
-            max_id = 0
-        return max_id
+        cur.execute("SELECT item_guid FROM rss WHERE title = ? ORDER BY id DESC LIMIT 1;", (title,))
+        row = cur.fetchone()
+        return int(row['item_guid']) if row else 0
 
     def create_table(self):
         # 테이블을 생성하는 메서드
-        sql = """ CREATE TABLE IF NOT EXISTS rss (
-                    id integer PRIMARY KEY AUTOINCREMENT,
-                    
-                    title text NOT NULL,
-                    link text NOT NULL,
-                    description text NOT NULL,
-                    
-                    item_title text NOT NULL,
-                    item_link text NOT NULL,
-                    item_description text NOT NULL,
-                    item_author text NOT NULL,
-                    item_category text NOT NULL,
-                    item_pubDate text NOT NULL,
-                    item_guid text NOT NULL
-                ); """
+        sql = """CREATE TABLE IF NOT EXISTS rss (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    title TEXT NOT NULL,
+                    link TEXT NOT NULL,
+                    description TEXT NOT NULL,
+                    item_title TEXT NOT NULL,
+                    item_link TEXT NOT NULL,
+                    item_description TEXT NOT NULL,
+                    item_author TEXT NOT NULL,
+                    item_category TEXT NOT NULL,
+                    item_pubDate TEXT NOT NULL,
+                    item_guid TEXT NOT NULL
+                );"""
         cur = self.conn.cursor()
         cur.execute(sql)
         self.conn.commit()
--- rssmaker/parser_mohw.py
+++ rssmaker/parser_mohw.py
@@ -25,10 +25,10 @@ class IssuesPublichearing(Issues):
         self.link = "https://www.mohw.go.kr/menu.es?mid=a10409030000"
         self.description = "보건복지부 입법/행정예고 전자공청회"
         main_content = _link[2].find('a')
-        self.item_title = _link[2].text.strip('\r').strip('\n').strip('\t').strip()
-        self.item_link = "https://www.epeople.go.kr/cmmn/idea/redirect.do?ideaRegNo={0}".format(re.search(r"([A-Z0-9]{3}-\d{4}-\d{7})", main_content.get('onclick')).group(1))        
-        self.item_description = "기간 {0}".format(_link[3].text)
-        self.item_author = _link[3].text.strip('\r').strip('\n').strip('\t').strip()
+        self.item_title = _link[2].get_text(strip=True)
+        self.item_link = f"https://www.epeople.go.kr/cmmn/idea/redirect.do?ideaRegNo={re.search(r'([A-Z0-9]{3}-\d{4}-\d{7})', main_content.get('onclick')).group(1)}"
+        self.item_description = f"기간 {_link[3].get_text(strip=True)}"
+        self.item_author = _link[3].get_text(strip=True)
         self.item_category = ""
         self.item_pubDate = datetime.datetime.now(datetime.UTC)
         self.item_guid = _link[0].text
@@ -59,13 +59,11 @@ class IssuesLaw(Issues):
         self.link = "https://www.mohw.go.kr/board.es?mid=a10409020000&bid=0026"
         self.description = "보건복지부 훈령/예규/고시/지침"
         main_content = _link[2].find('a')
-        self.item_title = _link[2].text.strip('\r').strip('\n').strip('\t').strip()
-        #self.item_link = main_content.get('href')
-        self.item_link = "https://www.law.go.kr/법령/{0}".format(self.item_title)
-        self.item_description = "공포일:{0}, 시행일:{1}".format(_link[3].text.strip('\r').strip('\n').strip('\t').strip(), _link[4].text.strip('\r').strip('\n').strip('\t').strip())
+        self.item_title = _link[2].get_text(strip=True)
+        self.item_link = f"https://www.law.go.kr/법령/{self.item_title}"
+        self.item_description = f"공포일:{_link[3].get_text(strip=True)}, 시행일:{_link[4].get_text(strip=True)}"
         self.item_author = "보건복지부"
         self.item_category = ""
         self.item_pubDate = datetime.datetime.now(datetime.UTC)
-        #self.item_guid = ''.join(filter(str.isdigit, _link[1].text))
         self.item_guid = re.search(r'MST=(\d+)', main_content.get('href')).group(1)
         return
